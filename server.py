4c6bdb5891bb8afafc236caafc19db68366514b3
iwxfer <iwxfer@gmail.com>
2022-04-08 17:51:37 -0500

    fix selenium

--- server.py
+++ server.py
@@ -329,4 +329,4 @@ def tgwebhook():
     print(data)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, ssl_context=('cert.pem', 'key.pem'), host='0.0.0.0', port=8080)
