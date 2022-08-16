b7f29ad9ec208cb71316ab9f73905cbc88874911
iwxfer <iwxfer@gmail.com>
2022-08-16 17:51:49 -0500

    fix sc

--- bot.py
+++ bot.py
@@ -139,7 +139,7 @@ def mainbot():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("auth", gcalauth))
     dp.add_handler(CommandHandler("schedule", schedule))
-    dp.add_handler(CommandHandler("sc", schedule))
+    dp.add_handler(CommandHandler("sd", schedule))
     dp.add_handler(CallbackQueryHandler(button))
 
     # log all errors
